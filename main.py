b8e7850dc7c65f9c573dbc902609b2391b70f90e
Sang-heon <choi.sg@obmontessori.edu.ph>
2025-09-22 16:26:05 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# Data Types in Python
+from pyscript import display
+
+# Variables
+business_hours = "Open: 12:00 PM - 9:00 PM" # String
+
+contact_us = "Contact Us"  # String
+contact_description = "We'd love to hear from you!"  # String
+location = "Our Location"  # String
+location_description1 = "123 Coffee Street"  # String
+location_description2 = "Brewville, GH San Juan"  # String
+location_description3 = "Philippines"  # String
+phone_number = "Phone Numbers"  # String
+phone_number_description1 = "Main: (123) 456-7890"  # String
+phone_number_description2 = "Mobile: (0912) 345-6789"  # String
+email_address = "Email Addresses"  # String
+email_address_description1 = "info@sweetchoi.com"  # String
+email_address_description2 = "career@sweetchoi.com"  # String
+email_address_description3 = "support@sweetchoi.com"  # String
+
+# Business Hours
+display(f"{business_hours}", target = "business_hours")
+
+# Contact Us
+display(f"{contact_us}", target = "contact_us")
+display(f"{contact_description}", target = "contact_description")
+
+display(f"{location}", target = "location")
+display(f"{location_description1}", target = "location_description1")
+display(f"{location_description2}", target = "location_description2")
+display(f"{location_description3}", target = "location_description3")
+
+display(f"{phone_number}", target = "phone_number")
+display(f"{phone_number_description1}", target = "phone_number_description1")
+display(f"{phone_number_description2}", target = "phone_number_description2")
+
+display(f"{email_address}", target = "email_address")
+display(f"{email_address_description1}", target = "email_address_description1")
+display(f"{email_address_description2}", target = "email_address_description2")
+display(f"{email_address_description3}", target = "email_address_description3")
