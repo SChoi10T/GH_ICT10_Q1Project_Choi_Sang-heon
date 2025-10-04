4926bd8b5cfbdf25867cbf5ae94561488c43f404
Sang-heon <choi.sg@obmontessori.edu.ph>
2025-10-04 11:49:15 +0800

    Update contact.py

--- contact.py
+++ contact.py
@@ -6,9 +6,9 @@ from pyscript import display # pyright: ignore[reportMissingImports]
 contact_us = "Contact Us"  # String
 contact_description = "We'd love to hear from you!"  # String
 location = "Our Location"  # String
-location_description1 = "123 Coffee Street"  # String
-location_description2 = "Brewville, GH San Juan"  # String
-location_description3 = "Philippines"  # String
+location_description1 = "M. Paterno Street"  # String
+location_description2 = "Brgy. Pasadena, San Juan City"  # String
+location_description3 = "Metro Manila, Philippines"  # String
 phone_number = "Phone Numbers"  # String
 phone_number_description1 = "Main: (123) 456-7890"  # String
 phone_number_description2 = "Mobile: (0912) 345-6789"  # String
@@ -37,3 +37,4 @@ display(f"{email_address_description2}", target = "email_address_description2")
 display(f"{email_address_description3}", target = "email_address_description3")
 
 display(f"{business_hours}", target = "business_hours")
+
